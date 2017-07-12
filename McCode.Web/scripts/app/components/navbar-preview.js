f231beadca24fc8de9ae7fe6b54b30553026ac4f
elgboffel <johnny2900@gmail.com>
2017-07-12 23:20:35 +0200

    Dev frontpage

--- McCode.Web/scripts/app/components/navbar-preview.js
+++ McCode.Web/scripts/app/components/navbar-preview.js
@@ -3,12 +3,44 @@ var App;
     var NavbarPreview = (function () {
         function NavbarPreview() {
             this.openDropdown();
+            //this.replacePlaceholder();
         }
         NavbarPreview.prototype.openDropdown = function () {
-            $('.navbar-preview__expander').on('click', function () {
+            var _this = this;
+            var $element = $('.navbar-preview__expander');
+            $element.on('click', function () {
                 var $this = $(this);
-                $this.find('.dropdown__content').slideToggle(250);
-                $this.toggleClass('dropdown--expanded');
+                $this.toggleClass('navbar-preview__expander--expanded');
+                _this.toggleNavbarHeight($('.navbar-preview'));
+            });
+        };
+        NavbarPreview.prototype.toggleNavbarHeight = function ($element) {
+            var $elementHeight = $element.outerHeight();
+            var $dropdownHeight = $element.find('ul').outerHeight();
+            var $preview = $element.find('.navbar-preview__preview').outerHeight();
+            if ($element.hasClass('js|open')) {
+                $element.css({ 'height': 63 });
+                $element.toggleClass('js|open');
+                setTimeout(function () {
+                    $('body').removeClass('overflow-hidden');
+                }, 225);
+            }
+            else {
+                $element.css({ 'height': $elementHeight + $dropdownHeight + $preview + 65 });
+                $element.toggleClass('js|open');
+                $('body').addClass('overflow-hidden');
+            }
+        };
+        NavbarPreview.prototype.replacePlaceholder = function () {
+            var $text = "";
+            $('.navbar-preview__expander').on('mouseover', function () {
+                var $this = $(this);
+                var $placeholder = $this.data().placeholder;
+                $text = $this.text();
+                $this.text($placeholder);
+            });
+            $('.navbar-preview__expander').on('mouseleave', function () {
+                $(this).text(this.$text);
             });
         };
         return NavbarPreview;
--- McCode.Web/scripts/app/components/navbar-preview.ts
+++ McCode.Web/scripts/app/components/navbar-preview.ts
@@ -2,14 +2,53 @@
     export class NavbarPreview {
         constructor() {
             this.openDropdown();
+            //this.replacePlaceholder();
         }
 
         openDropdown() {
-            $('.navbar-preview__expander').on('click', function () {
+            var _this = this;
+            var $element = $('.navbar-preview__expander');
+            $element.on('click', function () {
                 var $this = $(this);
-                $this.find('.dropdown__content').slideToggle(250);
-                $this.toggleClass('dropdown--expanded');
 
+                $this.toggleClass('navbar-preview__expander--expanded');
+                _this.toggleNavbarHeight($('.navbar-preview'));
+            });
+        }
+
+        toggleNavbarHeight($element: JQuery) {
+            var $elementHeight = $element.outerHeight();
+            var $dropdownHeight = $element.find('ul').outerHeight();
+            var $preview = $element.find('.navbar-preview__preview').outerHeight();
+
+            if ($element.hasClass('js|open')) {
+                $element.css({ 'height': 63 });
+                $element.toggleClass('js|open');
+                setTimeout(function () {
+                    $('body').removeClass('overflow-hidden');
+                }, 225);
+
+            } else {
+                $element.css({ 'height': $elementHeight + $dropdownHeight + $preview + 65 });
+                $element.toggleClass('js|open');
+                $('body').addClass('overflow-hidden');
+            }
+        }
+
+        replacePlaceholder() {
+            var $text = "";
+
+            $('.navbar-preview__expander').on('mouseover', function () {
+                var $this = $(this);
+                var $placeholder = $this.data().placeholder;
+                $text = $this.text();
+
+                $this.text($placeholder);
+            });
+
+            $('.navbar-preview__expander').on('mouseleave', function () {
+
+                $(this).text(this.$text);
             });
         }
     }
